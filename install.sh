4e4d66c0046deb43185c6df12634936372efd64a
DreamMan <67083550+nvhuy1509@users.noreply.github.com>
2024-03-30 01:41:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,15 +49,12 @@ gen_proxy_file_for_user() {
     cat >proxy.txt <<EOF
 $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
+ cat proxy.txt
 }
 
 upload_proxy() {
     local PASS=$(random)
-    zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl -s --upload-file proxy.zip https://anonfiles.com/upload)
-
-    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-    echo "Download zip archive from: ${URL}"
+   
     echo "Password: ${PASS}"
 
 }
